4e5c58c64d878ed46cf4181285b25d9c2b2a4604
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-08-16 18:45:37 +0200

    Trying JPDH with nar.

--- src/main/java/com/arkanosis/jpdh/JPDH.java
+++ src/main/java/com/arkanosis/jpdh/JPDH.java
@@ -1,17 +1,10 @@
 package com.arkanosis.jpdh;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import com.arkanosis.JPDH.NarSystem;
 
 public class JPDH {
     static {
-        try {
-            loadLibrary("jnijpdh");
-        } catch (IOException e) {
-            throw new ExceptionInInitializerError(e);
-        }
+        NarSystem.loadLibrary();
     }
 
     public static Query openQuery(String dataSource) throws JPDHException {
@@ -26,13 +19,4 @@ public class JPDH {
 
     private static native Query nOpenQuery(String dataSource) throws JPDHException;
 
-    private static void loadLibrary(String name) throws IOException {
-        File library = File.createTempFile(name, ".dll");
-        Files.copy(
-            JPDH.class.getResourceAsStream("/" + System.mapLibraryName(name)),
-            library.toPath(),
-            StandardCopyOption.REPLACE_EXISTING
-        );
-        System.load(library.getAbsolutePath());
-    }
 }
